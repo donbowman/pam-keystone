8985c6b469fbeca2e2752ef3aa54a9ace3525e58
don <don.waterloo@gmail.com>
2014-11-30 23:00:23 +0000

    switch to using urllib rather than keystone client (some issue w/ loading keystone client in the context of nginx in pam).

--- keystone-auth.py
+++ keystone-auth.py
@@ -2,8 +2,9 @@
 # Authenticate against OpenStack Keystone
 #
 
-import syslog, os, traceback
-from keystoneclient.v2_0 import client
+import syslog, os, traceback, sys
+import urllib2, json,hashlib
+import memcache
 
 def pam_sm_authenticate(pamh, flags, argv):
     try:
@@ -13,14 +14,40 @@ def pam_sm_authenticate(pamh, flags, argv):
 	    passmsg = pamh.Message(pamh.PAM_PROMPT_ECHO_OFF, "Keystone password")
 	    rsp = pamh.conversation(passmsg)
 	    pamh.authtok = rsp.resp
-
 	try:
-	    client.Client(username=pamh.user, password=pamh.authtok, auth_url="https://nubo.sandvine.rocks:5000/v2.0")
-	    syslog.syslog("pam keystone success for %s" % pamh.user)
-	    return pamh.PAM_SUCCESS
+            mu = hashlib.sha1()
+            mu.update(pamh.user)
+            mp = hashlib.sha1()
+            mp.update(pamh.authtok)
+            mc = memcache.Client([('127.0.0.1',11211)])
+            v = mc.get("%s-%s" % (mu.hexdigest(),mp.hexdigest()))
+            if v != None:
+                return pamh.PAM_SUCCESS
+
+            import json, urllib2
+            val = {
+                "auth": {
+                    "passwordCredentials": {
+                        "password": pamh.authtok,
+                        "username": pamh.user
+                    }
+                }
+            }
+            req = urllib2.Request('https://nubo-7.sandvine.rocks:5000/v2.0/tokens')
+            req.add_header('Content-Type', 'application/json')
+            try:
+                response = urllib2.urlopen(req, json.dumps(val))
+
+                if (response.getcode() == 200):
+                    mc.set("%s-%s" % (mu.hexdigest(),mp.hexdigest()),"true", 900)
+                    syslog.syslog("pam-keystone: user %s login" % pamh.user)
+                    return pamh.PAM_SUCCESS
+            except:
+                # Don't want this error, its the 401
+                pass
 	except:
-	    syslog.syslog("pam keystone fail for %s" % pamh.user)
-	    pass
+	    syslog.syslog("pam keystone fail for %s (%s)" % (pamh.user, traceback.format_exc()))
+            pass
     except:
 	syslog.syslog("Unhandled exception %s " % traceback.format_exc())
     return pamh.PAM_AUTH_ERR
